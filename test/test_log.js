646b37a1fcd8af8f723a5a13c7f0934c9e009eed
Kevin Ludwig <kevin.ludwig@gmail.com>
2020-03-31 09:19:57 -0700

    get test passing

--- test/test_log.js
+++ test/test_log.js
@@ -9,16 +9,16 @@ describe('superagent log', () => {
     it('should call log with params', async () => {
         nock('https://github.com')
             .get('/something')
-            .reply(500);
+            .reply(201);
 
         function logFn(obj) {
             obj.protocol.should.be.eql('HTTPS');
             obj.method.should.be.eql('GET');
             obj.href.should.be.eql('https://github.com/something');
-            obj.status.should.be.eql(500);
+            obj.status.should.be.eql(201);
             should.exist(obj.elapsed);
         }
 
-        await request.get('https://github.com/something').query({a: 1}).use(saLog(logFn));
+        await request.get('https://github.com/something').use(saLog(logFn));
     });
 });
